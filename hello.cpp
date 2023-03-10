d3a8b794feac898ed427105c2877ca8a880c283e
Fedor <fedorinter2004@gmail.com>
2023-03-10 18:20:23 +0300

    Fixed cpp file

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
  
-using namespace std;
-
-int main(int argc, char** argv) {
-   string name;
-   cin >> name;
-   cout << "Hello world from " << name << endl;
-}
+int main(int argc, char** argv){
+  string name;                                           // Имя пользователя
+  std::cin >> name;                                      // Ввод имени пользователя
+  std::cout << "Hello world from " << name << std::endl; // Вывод данных
+} 
